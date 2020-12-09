c3f7694ad0b8fcb92ce71dafc35681c5a1dab5bf
JensDiemer <git@jensdiemer.de>
2020-12-09 19:58:42 +0100

    Bugfix serving user media files

--- conf/ynh_urls.py
+++ conf/ynh_urls.py
@@ -5,6 +5,10 @@ from django.urls import path
 # settings.PATH_URL is the $YNH_APP_ARG_PATH
 if settings.PATH_URL:
     urlpatterns = [
+        # XXX: Hack - the MEDIA_URL contains the "PATH_URL" already:
+        path(settings.MEDIA_URL.lstrip('/'), include('django_tools.serve_media_app.urls')),
+
+        # Prefix all urls with "PATH_URL":
         path(f'{settings.PATH_URL}/', include('inventory_project.urls'))
     ]
 else:
