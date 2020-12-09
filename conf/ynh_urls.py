6adbe3eaef29de63a00db9a2ef2481317c65a3f7
JensDiemer <git@jensdiemer.de>
2020-12-09 13:31:25 +0100

    bugfix $YNH_APP_ARG_PATH handling

--- conf/ynh_urls.py
+++ conf/ynh_urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
 
-urlpatterns = [
-    path('__PATH_URL__/', include('inventory_project.urls'))
-]
+# settings.PATH_URL is the $YNH_APP_ARG_PATH
+if settings.PATH_URL:
+    urlpatterns = [
+        path(f'{settings.PATH_URL}/', include('inventory_project.urls'))
+    ]
+else:
+    # Installed to domain root, without a path prefix?
+    from inventory_project.urls import urlpatterns  # noqa
