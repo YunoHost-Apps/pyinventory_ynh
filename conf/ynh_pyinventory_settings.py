12a5adab54c53e24deb7f25ec8e03992989e9991
JensDiemer <git@jensdiemer.de>
2020-12-19 20:06:07 +0100

    bugfix import "local_settings.py"

--- conf/ynh_pyinventory_settings.py
+++ conf/ynh_pyinventory_settings.py
@@ -188,6 +188,6 @@ LOGGING = {
 # -----------------------------------------------------------------------------
 
 try:
-    from .local_settings import *  # noqa
+    from local_settings import *  # noqa
 except ImportError:
     pass
