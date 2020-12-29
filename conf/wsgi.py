465bf8b8eb7e268b1c38f91ba50857f595db0244
JensDiemer <git@jensdiemer.de>
2020-12-29 12:52:08 +0100

    rename settings/urls

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -2,7 +2,7 @@
     WSGI config
 """
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ynh_pyinventory_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
