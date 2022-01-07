bc68077827de0c9862023df75770d7cf89ea4ea0
tituspijean <tituspijean@outlook.com>
2022-01-07 22:47:18 +0100

    Debug venv upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -24,8 +24,10 @@ log_file="${log_path}/pyinventory.log"
 # COMMON VARIABLES
 #=================================================
 
+python_version="3.7"
+
 # dependencies used by the app
-pkg_dependencies="build-essential python3-dev python3-pip python3-venv git libpq-dev postgresql postgresql-contrib libjpeg-dev"
+pkg_dependencies="build-essential python${python_version}-dev python${python_version}-pip python${python_version}-venv git libpq-dev postgresql postgresql-contrib libjpeg-dev"
 
 #=================================================
 # Redis HELPERS
