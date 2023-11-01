5cdbb5dc648285a8c3228c9ff93c1a4ccaad6358
Jens Diemer <git@jensdiemer.de>
2023-11-01 20:19:13 +0100

    Update project -> migrate from poetry to pip-tools

---
+++ pyinventory_ynh/__init__.py
@@ -0,0 +1,7 @@
+"""
+    pyinventory_ynh
+    Web based management to catalog things including state and location etc.
+"""
+
+__version__ = '0.19.3+ynh1'
+__author__ = 'Jens Diemer <pyinventory_ynh@jensdiemer.de>'
